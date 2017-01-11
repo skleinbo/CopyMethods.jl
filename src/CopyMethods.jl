37f40af4062e9d5c17677dc430e3d147da75da24
Stephan Kleinboelting <skleinbo@thp.uni-koeln.de>
2017-01-11 15:58:57 +0100

    Enable slicing of rows/cols

--- src/CopyMethods.jl
+++ src/CopyMethods.jl
@@ -3,14 +3,14 @@ module CopyMethods
 
 export copy_col!, copy_row!, unit
 
-function copy_col!{T}(A::AbstractArray{T,2},col1::Int,col2::Int)
-  @inbounds @simd for i in 1:size(A,1)
+function copy_col!{T}(A::AbstractArray{T,2},col1::Int,col2::Int,row_min=0,row_max=Inf)
+  @inbounds @simd for i in max(1, row_min):min(size(A,1), row_max)
     A[i,col2] = A[i,col1]
   end
 end
 
-function copy_row!{T}(A::AbstractArray{T,2},row1::Int,row2::Int)
-  for i in 1:size(A,2)
+function copy_row!{T}(A::AbstractArray{T,2},row1::Int,row2::Int,col_min=0,col_max=Inf)
+  for i in max(1, col_min):min(size(A,2), col_max)
     @inbounds A[row2,i] = A[row1,i]
   end
 end
